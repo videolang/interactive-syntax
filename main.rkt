8ddc88ccf4e5a8390b921a260b26448c00350152
Leif Andersen <leif@leifandersen.net>
2017-10-08 13:36:33 -0700

    Split out font and label into text$.

--- main.rkt
+++ main.rkt
@@ -1,35 +1,473 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(provide (all-defined-out))
+(require file/convertible
+         pict
+         (prefix-in pict: pict)
+         racket/set
+         racket/list
+         racket/math
+         racket/draw
+         racket/class
+         racket/gui/base
+         racket/serialize
+         racket/stxparam
+         racket/splicing
+         racket/match
+         (for-syntax racket/base
+                     racket/syntax
+                     syntax/parse))
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(define serial-key (generate-member-key))
+(define deserial-key (generate-member-key))
+(define copy-key (generate-member-key))
 
-;; Code here
+(begin-for-syntax
+  (define-syntax-class defstate
+    #:literals (define-state)
+    (pattern (define-state name body ...)))
+  (define-syntax-class defpubstate
+    #:literals (define-public-state)
+    (pattern (define-public-state name body ...))))
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
+(define-syntax-parameter defstate-parameter
+  (syntax-parser
+    [(_ stx who)
+     (raise-syntax-error #'who "Use outside of define-idmt is an error" this-syntax)]))
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
+(define-syntax (define-state stx)
+  (syntax-parse stx
+    [x:defstate
+     #`(defstate-parameter #,stx define-state)]))
+
+(define-syntax (define-public-state stx)
+  (syntax-parse stx
+    [x:defpubstate
+     #`(defstate-parameter #,stx define-public-state)]))
+
+(define-syntax (~define-idmt stx)
+ (syntax-parse stx
+    [(_ orig-stx name:id supclss (interfaces ...)
+        (~or (~optional (~seq #:base? b?) #:defaults ([b? #'#f]))
+             (~optional (~seq #:direct-deserialize? dd?) #:defaults ([dd? #'#t])))
+        ...
+        (~and
+         (~seq (~or state:defstate public-state:defpubstate internal-body) ...)
+         (~seq body ...)))
+     #:with name-deserialize (format-id stx "~a:deserialize" #'name)
+     (define dd?* (syntax-e #'dd?))
+     (unless (or (not dd?*) (eq? 'module (syntax-local-context)))
+       (raise-syntax-error #f "Must be defined at the module level" #'orig-stx))
+     (define serialize-method (gensym 'serialize))
+     (define deserialize-method (gensym 'deserialize))
+     (define copy-method (gensym 'copy))
+     (define base? (syntax-e (attribute b?)))
+     #`(begin
+         (define-member-name #,serialize-method serial-key)
+         (define-member-name #,deserialize-method deserial-key)
+         (define-member-name #,copy-method copy-key)
+         #,@(if dd?*
+                (list
+                 #`(provide name-deserialize)
+                 #`(define name-deserialize
+                     (make-deserialize-info
+                      (λ (sup table public-table)
+                        (define this (new name))
+                        (send this #,deserialize-method (vector sup table public-table))
+                        this)
+                      (λ ()
+                        (define pattern (new name))
+                        (values pattern
+                                (λ (other)
+                                  (send pattern #,copy-method other)))))))
+                '())
+         (splicing-syntax-parameterize ([defstate-parameter
+                                          (syntax-parser
+                                            [(_ st:defstate who)
+                                             #'(field [st.name st.body (... ...)])]
+                                            [(_ st:defpubstate who)
+                                             #'(field [st.name st.body (... ...)])])])
+           (define name
+             (let ()
+               #,@(for/list ([i (in-list (attribute state.name))])
+                    #`(define-local-member-name #,i))
+               (class/derived
+                orig-stx
+                (name
+                 supclss
+                 ((interface* () ([prop:serializable
+                                   (make-serialize-info
+                                    (λ (this)
+                                      (send this #,serialize-method))
+                                    #'name-deserialize
+                                    #t
+                                    (or (current-load-relative-directory) (current-directory)))]))
+                  interfaces ...)
+                 #f)
+                (define (#,serialize-method)
+                  (vector #,(if base?
+                                #'#f
+                                #`(super #,serialize-method))
+                          (make-immutable-hash
+                           `#,(for/list ([i (in-list (attribute state.name))])
+                                #`(#,(syntax->datum i) . ,#,i)))
+                          (make-immutable-hash
+                           `#,(for/list ([i (in-list (attribute public-state.name))])
+                                #`(#,(syntax->datum i) . ,#,i)))))
+                (#,(if base? #'public #'override) #,serialize-method)
+                (define (#,deserialize-method data)
+                  (define sup (vector-ref data 0))
+                  (define table (vector-ref data 1))
+                  (define public-table (vector-ref data 2))
+                  #,(if base?
+                        #`(void)
+                        #`(super #,deserialize-method sup))
+                  #,@(for/list ([i (in-list (attribute state.name))])
+                       #`(set! #,i (hash-ref table '#,(syntax->datum i))))
+                  #,@(for/list ([i (in-list (attribute public-state.name))])
+                       #`(set! #,i (hash-ref public-table '#,(syntax->datum i)))))
+                (#,(if base? #'public #'override) #,deserialize-method)
+                (define (#,copy-method other)
+                  #,(if base?
+                        #`(void)
+                        #`(super #,copy-method other))
+                  #,@(for/list ([i (in-list (attribute state.name))])
+                       #`(set! #,i (get-field #,i other)))
+                  #,@(for/list ([i (in-list (attribute public-state.name))])
+                       #`(set! #,i (get-field #,i other)))
+                  (void))
+                (#,(if base? #'public #'override) #,copy-method)
+                body ...)))))]))
+
+(define-syntax (define-base-idmt* stx)
+  (syntax-parse stx
+    [(_ name:id super (interfaces ...) body ...)
+     #`(~define-idmt #,stx name super (interfaces ...) #:base? #t body ...)]))
+
+(define-syntax (define-idmt stx)
+  (syntax-parse stx
+    [(_ name:id super
+        (~or (~optional (~seq #:interfaces (interfaces ...)) #:defaults ([(interfaces 1) '()])))
+        ...
+        body ...)
+     #`(~define-idmt #,stx name super (interfaces ...) body ...)]))
+
+(define-syntax (define-idmt-mixin stx)
+  (syntax-parse stx
+    [(_ name:id
+        (~or (~optional (~seq #:interfaces (interfaces ...)) #:defaults ([(interfaces 1) '()]))
+             (~optional (~seq #:mixins (mixins ...)) #:defaults ([(mixins 1) '()])))
+        ...
+        body ...)
+     #`(begin
+         (define (name $)
+           (~define-idmt #,stx
+                         name
+                         ((compose #,@(reverse (attribute mixins))) $)
+                         (interfaces ...)
+                         #:direct-deserialize? #f
+                         body ...)
+           name))]))
+
+(define idmt<$>
+  (interface*
+   ()
+   ([prop:convertible
+     (λ (this format default)
+       (case format
+         [(png-bytes)
+          (define-values (w* h*) (send this get-min-extent))
+          (define w (exact-ceiling (max w* 1)))
+          (define h (exact-ceiling (max h* 1)))
+          (define bit (make-object bitmap% w h))
+          (send this draw (new bitmap-dc% [bitmap bit]) 0 0 w h)
+          (define s (open-output-bytes))
+          (send bit save-file s 'png)
+          (get-output-bytes s)]
+         [else default]))])
+   get-min-extent
+   get-max-extent
+   set-current-extent
+   get-current-extent
+   draw
+   on-mouse-event
+   on-keyboard-event))
+
+(define-base-idmt* base$ object% (idmt<$>)
+  (super-new)
+  (define-state x #f)
+  (define-state y #f)
+  (define-state width #f)
+  (define-state height #f)
+  (define/public (add-data key val)
+    (void))
+  (define/public (draw dc x y w h)
+    (void))
+  (define/public (get-min-extent)
+    (values 0 0))
+  (define/public (on-mouse-event event)
+    (void))
+  (define/public (on-keyboard-event event)
+    (void))
+  (define/public (get-max-extent)
+    (values +inf.0 +inf.0))
+  (define/public (set-current-extent nx ny nw nh)
+    (set! x nx)
+    (set! y ny)
+    (set! width nw)
+    (set! height nh))
+  (define/public (get-current-extent)
+    (values x y width height)))
+
+(define-idmt-mixin receiver$
+  (super-new)
+  (define/public (on-receive event)
+    (void))
+  (define/public (signal event)
+    (on-receive event)))
+
+(define-idmt-mixin signaler$
+  (super-new)
+  (define-public-state receivers (mutable-set))
+  (define/public (signal event)
+    (for ([r (in-set receivers)])
+      (send r signal event)))
+  (define/public (register-receiver x)
+    (set-add! receivers x))
+  (define/public (unregister-receiver x)
+    (set-remove! receivers x)))
+
+(define idmt-canvas%
+  (class canvas%
+    (init-field idmt)
+    (define-values (init-min-width init-min-height)
+      (send idmt get-min-extent))
+    (super-new [min-width (exact-ceiling init-min-width)]
+               [min-height (exact-ceiling init-min-height)]
+               [paint-callback (λ (c dc)
+                                 (send idmt set-current-extent 0 0 (send c get-width) (send c get-height))
+                                 (send idmt draw dc 0 0 (send c get-width) (send c get-height))
+                                 (define-values (w h) (send idmt get-min-extent))
+                                 (send this min-width (exact-ceiling w))
+                                 (send this min-height (exact-ceiling h)))])
+    (define/override (on-event event)
+      (send idmt on-mouse-event event)
+      (send this refresh))
+    (define/override (on-char event)
+      (send idmt on-keyboard-event event)
+      (send this refresh))))
+
+(define-idmt widget$ base$
+  (super-new)
+  ;(init-field [font normal-control-font])
+  (init [(internal-font font) normal-control-font])
+  (define-state font '())
+  (define/public (get-font)
+    (apply make-object font% font))
+  (define/public (set-font f)
+    (set! font (list (send f get-size)
+                     (send f get-face)
+                     (send f get-family)
+                     (send f get-style)
+                     (send f get-weight)
+                     (send f get-underlined)
+                     (send f get-smoothing)
+                     (send f get-size-in-pixels)
+                     (send f get-hinting))))
+  (set-font internal-font)
+  (define-public-state vert-margin 2)
+  (define-public-state horiz-margin 2)
+  (define-public-state style '())
+  (define-public-state parent #f)
+  (define/override (get-min-extent)
+    (values (* 2 vert-margin) (* 2 horiz-margin)))
+  (define/public (register-parent other)
+    (set! parent other))
+  (define/public (get-child-extent child)
+    (error 'get-child-extent "IDMT does not have children")))
+
+(define-idmt list-widget$ widget$
+  (inherit/super get-min-extent)
+  (inherit/super set-current-extent)
+  (super-new)
+  (define-public-state idmt-list '())
+  (define/public (add-idmt idmt)
+    (set! idmt-list (append idmt-list (list idmt)))
+    (send idmt register-parent this))
+  (define/public (remove-idmt idmt)
+    (when (empty? idmt-list)
+      (error 'remove-idmt "List widget already emtpy"))
+    (send idmt register-parent #f)
+    (set! idmt-list (take idmt-list (sub1 (length idmt-list)))))
+  (define/public (get-get-min-extent w-compose h-compose)
+    (define-values (base-w base-h)
+      (super get-min-extent))
+    (define-values (w h)
+      (for/fold ([width 0]
+                 [height 0])
+                ([i (in-list idmt-list)])
+        (define-values (w h)
+          (send i get-min-extent))
+        (values (w-compose w width)
+                (h-compose h height))))
+    (values (+ base-w w) (+ base-h h)))
+  (define/public (get-draw x-acc y-acc item-width item-height dc x y w h)
+    (for/fold ([x x]
+               [y y])
+              ([i (in-list idmt-list)])
+      (send i draw dc x y item-width item-height)
+      (values (+ x-acc x) (+ y-acc y)))
+    (void))
+  (define/override (on-mouse-event event)
+    (super on-mouse-event event)
+    (for/list ([i (in-list idmt-list)])
+      (send i on-mouse-event event)))
+  (define/public (get-item-dim h)
+    (if (empty? idmt-list) #f (/ h (length idmt-list))))
+  (define/public (get-set-current-extent x-acc y-acc item-width item-height x y w h)
+    (super set-current-extent x y w h)
+    (for/fold ([x x]
+               [y y])
+              ([i (in-list idmt-list)])
+      (send i set-current-extent x y item-width item-height)
+      (values (+ x-acc x) (+ y-acc y)))
+    (void)))
+
+(define-idmt vertical-block$ list-widget$
+  (super-new)
+  (inherit-field idmt-list)
+  (define/override (get-min-extent)
+    (send this get-get-min-extent max +))
+  (define/override (draw dc x y w h)
+    (define item-height (send this get-item-dim h))
+    (send this get-draw 0 item-height w item-height
+          dc x y w h))
+  (define/override (set-current-extent x y w h)
+    (define item-height (send this get-item-dim h))
+    (send this get-set-current-extent 0 item-height w item-height x y w h)))
+
+(define-idmt horizontal-block$ list-widget$
+  (super-new)
+  (inherit-field idmt-list)
+  (define/override (get-min-extent)
+    (define-values (b-w b-h)
+      (super get-min-extent))
+    (define-values (w h)
+      (for/fold ([width 0]
+                 [height 0])
+                ([i (in-list idmt-list)])
+        (define-values (w h)
+          (send i get-min-extent))
+        (values (+ w width)
+                (max h height))))
+    (values (+ b-w w) (+ b-h h)))
+  (define/override (draw dc x y w h)
+    (define item-width (if (empty? idmt-list) #f (/ w (length idmt-list))))
+    (for/fold ([x x])
+              ([i (in-list idmt-list)])
+      (send i draw dc x y item-width h)
+      (values (+ x item-width)))
+    (void)))
+
+(define-idmt-mixin text$
+  (super-new)
+  (inherit-field horiz-margin
+                 vert-margin)
+  (define-public-state text "")
+  (define/override (get-min-extent)
+    (define the-font (send this get-font))
+    (define-values (b-w b-h)
+      (super get-min-extent))
+    (define pic (pict:text (or text "---") the-font))
+    (values (+ b-w (pict-width pic)) (+ b-h (pict-height pic))))
+  (define/override (draw dc x y w h)
+    (define old-font (send dc get-font))
+    (send dc set-font (send this get-font))
+    (send dc draw-text (or text "---") (+ horiz-margin x) (+ vert-margin y))
+    (send dc set-font old-font)))
+
+(define-idmt label$ (text$ widget$)
+  (inherit-field text)
+  (super-new)
+  (init [(internal-text text) #f])
+  (set! text internal-text))
+
+(define-idmt button$ (signaler$ widget$)
+  (super-new)
+  (inherit-field horiz-margin
+                 vert-margin)
+  (init [(internal-label label) (new label$)])
+  (define mouse-state 'up)
+  (define-state label* internal-label)
+  (define-state up-color "WhiteSmoke")
+  (define-state hover-color "Gainsboro")
+  (define-state down-color "LightGray")
+  (define/override (on-mouse-event event)
+    (define-values (x y w h)
+      (send this get-current-extent))
+    (define x-max (+ x w))
+    (define y-max (+ y h))
+    (define mouse-x (send event get-x))
+    (define mouse-y (send event get-y))
+    (define in-button?
+      (and (<= x mouse-x x-max)
+           (<= y mouse-y y-max)))
+    (match (send event get-event-type)
+      ['left-down
+       (when (and in-button? (eq? mouse-state 'hover))
+         (set! mouse-state 'down))]
+      ['left-up
+       (when (and in-button? (eq? mouse-state 'down))
+         (if in-button?
+             (set! mouse-state 'hover)
+             (set! mouse-state 'up))
+         (send this signal this))]
+      ['motion
+       (match mouse-state
+         [(or 'up 'hover)
+          (if in-button?
+           (set! mouse-state 'hover)
+           (set! mouse-state 'up))]
+         ['down
+          (unless in-button?
+            (set! mouse-state 'up))])]
+      [_ (void)]))
+  (define/override (get-min-extent)
+    (define-values (b-w b-h)
+      (super get-min-extent))
+    (define-values (w h)
+      (send label* get-min-extent))
+    (values (+ b-w w) (+ b-h h)))
+  (define/override (draw dc x y w h)
+    (define old-brush (send dc get-brush))
+    (send dc set-brush
+          (new brush% [color (make-object color%
+                               (match mouse-state
+                                 ['up up-color]
+                                 ['hover hover-color]
+                                 ['down down-color]))]))
+    (send dc draw-rectangle
+          (+ x horiz-margin) (+ y vert-margin)
+          (- w (* 2 horiz-margin)) (- h (* 2 vert-margin)))
+    (send label* draw dc
+          (+ x horiz-margin) (+ y vert-margin)
+          (- w (* 2 horiz-margin)) (- h (* 2 vert-margin)))
+    (send dc set-brush old-brush)))
+
+(define-idmt toggle$ widget$
+  (super-new))
+
+(define-idmt radio$ list-widget$
+  (super-new))
+
+(define-idmt field$ widget$
+  (super-new)
+  (define-state content "")
+  (define-state caret 0)
+  (define/override (on-keyboard-event event)
+    (define char (send event get-key-code))
+    (when (char? char)
+      (format "~a~a~a"
+              (substring content 0 caret)
+              char
+              (substring content caret)))))
