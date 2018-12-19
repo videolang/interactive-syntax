536fb2aea45d86fdd704d7de15adad5b0689c7e4
Leif Andersen <leif@leifandersen.net>
2018-12-19 14:59:58 -0500

    Major overhal of repo name.
    Mostly just turn IDMT (old name) to interactive-syntax (new name).
    Also restructure the repo so that I could uploaded it to the package server.

--- editor/info.rkt
+++ editor/info.rkt
@@ -1,17 +1,8 @@
 #lang info
+
 (define collection "editor")
-(define deps '(("base" "6.3")
-               "syntax-color-lib"
-               "draw-lib"
-               "gui-lib"
-               "images-lib"
-               "wxme-lib"
-               "drracket-plugin-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 ;(define scribblings '(("scribblings/idmt.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(leif))
+
 (define drracket-tools '(("private/surrogate.rkt")))
 (define drracket-tool-name '("Editor"))
 (define drracket-tool-icons '(#f))
---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '(("base" "7.1")))
+(define build-deps '("scribble-lib"
+                     "racket-doc"))
+(define version "0.0")
+(define pkg-authors '(leif))
+(define pkg-desc "Interactive Syntax")
